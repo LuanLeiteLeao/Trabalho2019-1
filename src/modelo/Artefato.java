f96ff5c66d6a2ff91356ad7104e3ad74642efd2d
luanleiteleao <luanleiteleao@luanleiteleao>
2019-09-18 15:40:22 -0300

    modelo artefato criado

---
+++ src/modelo/Artefato.java
@@ -0,0 +1,41 @@
+package modelo;
+
+public class Artefato {
+	private String    nome;
+	private int       quantidade;
+	private String    tipo;
+	private String    urlImagem;
+	private String    descricao;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
