2c2a31f5dbaa173899d12c6ef6d2dfab4aea3acd
Yuri Eguti <yurieguti@hotmail.com>
2019-09-26 20:48:09 -0300

    teste

--- src/visao/Formulario.java
+++ src/visao/Formulario.java
@@ -25,7 +25,7 @@ public class Formulario extends JFrame {
 	private JTable table;
 	Controle c = new Controle();
 	List<Artefato> artlist = c.selectArtefato();
-	ArtefatoTable modelo = new ArtefatoTable();
+	ArtefatoTable modelo = new ArtefatoTable(artlist);
 
 	/**
 	 * Launch the application.
